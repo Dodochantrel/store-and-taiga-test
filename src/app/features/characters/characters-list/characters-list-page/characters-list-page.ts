bd2b49454b77b3ecb4733c56ea9bbf9425a7557c
dchantrel <dchantrel@portalp.com>
2026-07-06 11:14:05 +0000

    Av

--- src/app/features/characters/characters-list/characters-list-page/characters-list-page.ts
+++ src/app/features/characters/characters-list/characters-list-page/characters-list-page.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, effect, inject, signal } from '@angular/core';
 import { CharacterStore } from '../../character-store';
 import { TuiButton, TuiDataList, TuiIcon, TuiInput, TuiTitle } from '@taiga-ui/core';
 import { TuiCardLarge, TuiHeader } from '@taiga-ui/layout';
@@ -6,6 +6,8 @@ import { TuiChevron, TuiDataListWrapper, TuiLike, TuiPagination, TuiSelect } fro
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { characterGenders } from '../../../../shared/resources/character-genders';
 import { FormsModule } from '@angular/forms';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { debounceTime, distinctUntilChanged, Subject } from 'rxjs';
 
 @Component({
   selector: 'app-characters-list-page',
@@ -31,6 +33,25 @@ import { FormsModule } from '@angular/forms';
 })
 export class CharactersListPage {
   protected readonly store: InstanceType<typeof CharacterStore> = inject(CharacterStore);
+  protected readonly searchValue = signal('');
+  private readonly searchTermInput$ = new Subject<string>();
 
   protected characterGenders = characterGenders;
+
+  constructor() {
+    effect(() => {
+      this.searchValue.set(this.store.searchTerm());
+    });
+
+    this.searchTermInput$
+      .pipe(debounceTime(500), distinctUntilChanged(), takeUntilDestroyed())
+      .subscribe((searchTerm) => {
+        this.store.setSearchTerm(searchTerm);
+      });
+  }
+
+  protected onSearchInput(searchTerm: string): void {
+    this.searchValue.set(searchTerm);
+    this.searchTermInput$.next(searchTerm);
+  }
 }
